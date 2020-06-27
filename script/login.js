6b32f487c45173b292d526be7c78e75145ec6b70
M Ilham Fauzi <67301198+Ilham-Fauzi02@users.noreply.github.com>
2020-06-27 12:31:47 +0700

    Update login.js

--- script/login.js
+++ script/login.js
@@ -14,6 +14,7 @@ function onSignOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
+        hideDashboard();
     });
 }
 
